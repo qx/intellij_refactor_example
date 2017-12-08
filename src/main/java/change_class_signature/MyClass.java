d45442cb4cee2fb9f1317ffdc1c30765095e238c
44708869@qq.com <44708869@qq.com>
2017-12-08 22:48:50 +0800

    Fri Dec  8 22:48:50 CST 2017

---
+++ src/main/java/change_class_signature/MyClass.java
@@ -0,0 +1,8 @@
+package change_class_signature;// This is the class whose signature will be changed:
+
+public class MyClass {
+    // some code here
+}
+
+// Now we are going to add two formal type parameters
+// to MyClass.
---
+++ src/main/java/change_class_signature/MyOtherClass.java
@@ -0,0 +1,12 @@
+package change_class_signature;
+
+public class MyOtherClass {
+
+    // Here are the references to MyClass:
+
+    MyClass myClass;
+    void myMethod(MyClass myClass) {
+        // some code here
+    }
+    // some code here
+}
