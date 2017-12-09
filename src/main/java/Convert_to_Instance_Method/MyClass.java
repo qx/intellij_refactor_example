6aa0739f5159bd4889d7bb4c6bb5c682a00181be
44708869@qq.com <44708869@qq.com>
2017-12-09 11:56:23 +0800

    Sat Dec  9 11:56:23 CST 2017

--- src/main/java/Convert_to_Instance_Method/MyClass.java
+++ src/main/java/Convert_to_Instance_Method/MyClass.java
@@ -3,7 +3,7 @@ package Convert_to_Instance_Method;
 public class MyClass {
     ClassA classA = new ClassA();
     ClassB classB = new ClassB();
-
+//Select static method to refactor
     static public void greatMethod(ClassA classA, ClassB classB) {
         System.out.println("ClassA=" + classA);
         System.out.println("ClassB=" + classB);
