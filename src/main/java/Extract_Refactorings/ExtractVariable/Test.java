6eb105c93bb2d8b85427b8893438806ef984c394
44708869@qq.com <44708869@qq.com>
2017-12-09 16:13:01 +0800

    comment extract variable

--- src/main/java/Extract_Refactorings/ExtractVariable/Test.java
+++ src/main/java/Extract_Refactorings/ExtractVariable/Test.java
@@ -2,12 +2,21 @@ package Extract_Refactorings.ExtractVariable;
 
 import Extract_Refactorings.ExtractField.AnotherClass;
 
+/**
+ * The Extract Variable refactoring puts the result of the selected expression into a variable. It declares a new variable and uses the expression as an initializer. The original expression is replaced with the new variable (see the examples below).
+ public void method() {
+ int a = 1;
+ int number = anotherClass.intValue();
+ int b = a + number;
+ int c = b + number;
+ }
+ */
 public class Test {
     private AnotherClass anotherClass;
 
     public void method() {
         int a = 1;
-        int b = a + anotherClass.intValue();
+        int b = a + anotherClass.intValue();//anotherClass.intValue() refactor this
         int c = b + anotherClass.intValue();
     }
 }
