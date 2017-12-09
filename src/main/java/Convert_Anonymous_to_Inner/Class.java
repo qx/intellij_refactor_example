36081757e092c75749409f24e480d8464b082e1e
44708869@qq.com <44708869@qq.com>
2017-12-09 11:54:10 +0800

    Sat Dec  9 11:54:10 CST 2017

--- src/main/java/Convert_Anonymous_to_Inner/Class.java
+++ src/main/java/Convert_Anonymous_to_Inner/Class.java
@@ -1,9 +1,10 @@
 package Convert_Anonymous_to_Inner;
 
-public class ClassA {
+
+public class Class {
     public Interface method() {
         final int i = 0;
-        return new Interface() {
+        return new Interface() {//curse to here
             public int publicMethod() {
             return i;}
         };
